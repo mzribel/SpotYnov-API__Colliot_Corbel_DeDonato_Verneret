b3de6530d2bad747c82c73e18d279837c9f7798f
RathGate <mariannecorbel@hotmail.fr>
2025-03-02 23:19:40 +0100

    added the route to tony's heart <3

--- SpotYnov API/app.ts
+++ SpotYnov API/app.ts
@@ -10,6 +10,7 @@ require('express-async-errors');
 require('dotenv').config();
 
 const app = express();
+const router = express.Router();
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -27,6 +28,7 @@ import authRoutes from './routes/auth.routes';
 import spotifyAuthRoutes from './routes/spotify.auth.routes';
 import userRoutes from './routes/user.routes';
 import groupRoutes from './routes/group.routes';
+import {ResponseService} from "./services/api/response.service";
 
 app.use('/auth', authRoutes);
 app.use('/auth/spotify', spotifyAuthRoutes);
@@ -34,10 +36,16 @@ app.use('/users', userRoutes);
 app.use('/groups', groupRoutes);
 // Swagger
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Tony
+router.get('/route_vers_le_coeur_de_tony', (req: Request, res: Response) => {
+    ResponseService.handleErrorResponse(res, 404, "🤌❤️");
+});
+app.use("/", router);
 // 404
 app.use((req:Request, res:Response):void => {
     throw new ApiError(404, "Not Found");
 })
+
 
 // @ts-ignore
 app.use(handleErrors)
